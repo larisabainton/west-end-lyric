29de4092cd78f8a6493db814b40f1f370b849f69
Larisa Bainton <larisabainton@gmail.com>
2023-09-08 18:12:20 -0400

    create and style contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 
 class Contact extends React.Component {
     render() {
-        return (<div></div>);
+        return (<div className="contact">
+            <ul className="contact-list">
+                <li className="contact-list_email contact-list_item">
+                    <div className="contact-list_title">Email</div>
+                    <div className="contact-list_text">blochperformingarts@gmail.com</div>
+                </li>
+                <li className="contact-list_social-media contact-list_item">
+                    <div className="contact-list_title">Connect</div>  
+                </li>
+            </ul>
+        </div>);
     }
 }
 
