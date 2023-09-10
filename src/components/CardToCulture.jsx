191a092c2076f7552116a1d578c0afbfac45a2ed
Larisa Bainton <larisabainton@gmail.com>
2023-09-10 17:33:42 -0400

    add ctc links

--- src/components/CardToCulture.jsx
+++ src/components/CardToCulture.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
+const cardToCulture = <a href="https://massculturalcouncil.org/organizations/card-to-culture/"> Card to Culture </a>;
+const massCulturalCouncil = <a href="https://massculturalcouncil.org/">Mass Cultural Council</a>;
+const departmentTransitionalAssistance = <a href="https://www.mass.gov/orgs/women-infants-children-nutrition-program">Department of Transitoinal Assistance</a>;
+const massHealthConnector = <a href="https://www.mahealthconnector.org/"> Massachusetts Health Connector</a>;
+const wicNutritionProgram = <a href="https://www.mass.gov/orgs/women-infants-children-nutrition-program"> Women, Infants, & Children (WIC) Nutrition Program</a>;
+const ebt = <a href="https://www.mass.gov/info-details/ebt-card-to-culture-organizations">EBT</a>;
+const wic = <a href="https://www.mass.gov/info-details/get-discounts-with-your-wic-card#cultural-organizations-">WIC</a>;
+const connectorCare= <a href="https://www.mahealthconnector.org/learn/plan-information/connectorcare-plans/connectorcare-card-to-culture">Connector Care</a>;
+
 
 class CardToCulture extends React.Component {
     render() {
@@ -7,13 +16,12 @@ class CardToCulture extends React.Component {
         <div className="card-to-culture">
             <div className="card-to-culture_title">Card to Culture</div>
             <div className="card-to-culture_text">
-                <p>West End Lyric is pleased to participate in the Card to Culture program, a collaboration between Mass Cultural Council and the Department of Transitional Assistance, the Massachusetts Health Connector, and the Women, Infants & Children (WIC) Nutrition Program by extending discounts to EBT, WIC, and ConnectorCare cardholders.</p>
+                <p>West End Lyric is pleased to participate in the {cardToCulture} program, a collaboration between {massCulturalCouncil} and the {departmentTransitionalAssistance}, the {massHealthConnector}, and the {wicNutritionProgram} by extending discounts to EBT, WIC, and Connector Care cardholders.</p>
                 <ul>
-                    <li>For admission-based performances, admission is free with an eligible EBT card.</li>
-                    <li>For admission-based performances, admission is free with an eligible WIC card.</li>
-                    <li>For admission-based performances, admission is $5 (unlimited) with a ConnectorCare card.</li>
+                    <li>For admission-based performances, admission is free with an eligible {ebt} card.</li>
+                    <li>For admission-based performances, admission is free with an eligible {wic} card.</li>
+                    <li>For admission-based performances, admission is $5 (unlimited) with a {connectorCare} card.</li>
                 </ul>
-                <p>See the complete list of participating organizations offering EBT, WIC, and ConnectorCare discounts.</p>
             </div>
         </div>);
     }
