241b1be2c4de4ea5eb3ea6caba1f661680c9884e
Larisa Bainton <larisabainton@gmail.com>
2023-09-16 11:16:01 -0400

    create donate button

---
+++ src/components/donateButton.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function DonateButton () {
+    return (
+        <a className="donate-button" href="https://bostonsynagogue.org/west-end-lyric/" target="_blank">Donate</a>
+    )
+}
