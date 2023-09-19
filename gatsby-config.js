eeac377bd15efb515b69332058053f29ef83f4fc
Larisa Bainton <larisabainton@gmail.com>
2023-09-19 11:38:33 -0400

    add gatsby source filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,5 +32,16 @@ module.exports = {
           }
         ],
       },
-    },],
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        // The unique name for each instance
+        name: `images`,
+        // Path to the directory
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
+    
 }
