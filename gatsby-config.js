fb1f10f125dda03c03c71782eed80f68558f29ad
Larisa Bainton <larisabainton@gmail.com>
2023-09-19 14:20:22 -0400

    fix images in production

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,36 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  pathPrefix: '/west-end-lyric', // https://www.gatsbyjs.com/docs/how-to/previews-deploys-hosting/how-gatsby-works-with-github-pages/
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://www.westendlyric.com`,
+    teamMembers: [
+      {
+        name: 'Jessica Bloch-Moisand',
+        jobTitle: 'Creative Producer',
+        headshot: '../images/headshots/Jessica_headshot.jpg',
+      },
+      {
+        name: 'Larisa Bainton',
+        jobTitle: 'Director of Development',
+        headshot: '../images/headshots/Larisa_headshot.jpg',
+      }, 
+      {
+        name: 'Hannah Shanefield',
+        jobTitle: 'Director of Marketing & Media',
+        headshot: '../images/headshots/Hannah_headshot.jpg',
+      },
+      {
+        name: 'Akela Franklin',
+        jobTitle: 'Director of Education & Outreach',
+        headshot: '../images/headshots/Akela_headshot.jpg',
+      },
+      {
+        name: 'Julia Pottinger',
+        jobTitle: 'Production Manager',
+        headshot: '../images/headshots/Julia_headshot.jpg',
+      }
+    ]
   },
   plugins: [
     'gatsby-plugin-sass',
