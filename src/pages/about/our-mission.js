5000ed80e5b400489424ec7eb97c516ff2dd51b5
Larisa Bainton <larisabainton@gmail.com>
2023-11-02 10:58:49 -0400

    create our mission page

--- src/pages/about/our-mission.js
+++ src/pages/about/our-mission.js
@@ -1,15 +1,39 @@
 import React from "react";
 
 import Layout from '../../components/layout';
+import { graphql, useStaticQuery } from "gatsby";
+import { renderRichText } from "gatsby-source-contentful/rich-text";
 
 const OurMissionPage = () => {
-    return(
+    const missionData = useStaticQuery(graphql`
+        query MissionPageQuery {
+            contentfulDescription(name: {eq: "Mission Statement"}) {
+                shortDescription {
+                  raw
+                }
+                longDescription {
+                    raw
+                }
+              }
+        }
+    `)
+
+    const { shortDescription, longDescription } = missionData.contentfulDescription;
+    
+    return (
         <Layout>
             <main>
-                <div>Hello</div>
+                <div className="our-mission">
+                    <div className="section-title">Our Mission</div>
+                    {renderRichText(shortDescription)}
+                    <div className="section-title">Our Core Values</div>
+                    {renderRichText(longDescription)}
+                </div>
             </main>
         </Layout>
     )
 }
 
 export default OurMissionPage;
+
+export const Head = () => <title>West End Lyric | Our Mission</title>
