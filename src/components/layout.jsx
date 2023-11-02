3af3d99b4951374a463117b239d16974a7b45233
Larisa Bainton <larisabainton@gmail.com>
2023-11-02 09:42:23 -0400

    create sub about menu

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -7,14 +7,20 @@ const Layout = ({ children }) => {
     return (
         <div className="global-wrapper">
             <div className="header" id="home">
-                <StaticImage src='../images/logo.png' alt='West End Lyric Logo' className="header_logo"/>
-                <div className="header_link-wrapper">
-                    <DonateButton className="header_donate-button"/>
-                    <Link to="/#home" className="header_link">Home</Link>
-                    <Link to="/#aboutUs" className="header_link">About</Link>
-                    <Link to="/#events" className="header_link">Events</Link>
-                    <Link to="/#contact" className="header_link">Contact</Link>
-                </div>
+                <Link to="/"><StaticImage src='../images/logo.png' alt='West End Lyric Logo' className="header_logo"/></Link>
+                <ul className="header_link-wrapper">
+                    <li><DonateButton className="header_donate-button"/></li>
+                    <li className="header_link-wrapper_list-item"><Link to="/#home" className="header_link">Home</Link></li>
+                    <li className="header_link-wrapper_list-item"><Link to="/#aboutUs" className="header_link">About</Link>
+                        <ul className="header_sub-menu">
+                            <li><Link className="header_sub-link" to="/about/our-mission">Our Mission</Link></li>
+                            <li><Link className="header_sub-link" to="/about/our-team">Our Team</Link></li>
+                            <li><Link className="header_sub-link" to="/about/our-sponsors">Our Sponsors</Link></li>
+                        </ul>
+                    </li>
+                    <li className="header_link-wrapper_list-item"><Link to="/#events" className="header_link">Events</Link></li>
+                    <li className="header_link-wrapper_list-item"><Link to="/#contact" className="header_link">Contact</Link></li>
+                </ul>
             </div>
             {children}
             <footer>© {new Date().getFullYear()} Larisa Bainton</footer>
---
+++ src/pages/about/our-mission.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Layout from '../../components/layout';
+
+const OurMissionPage = () => {
+    return(
+        <Layout>
+            <main>
+                <div>Hello</div>
+            </main>
+        </Layout>
+    )
+}
+
+export default OurMissionPage;
