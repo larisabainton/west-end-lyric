a0448cb8e9e03e7cd726951693231c936622c7e3
Larisa Bainton <larisabainton@gmail.com>
2023-11-11 21:19:22 -0500

    create contact section

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -7,9 +7,41 @@ const ContactPage = () => {
     return (
         <Layout>
             <main className="contact-page">
-                <div className="contact-page_signup-form">
-                <iframe title="mailing list form" src="https://docs.google.com/forms/d/e/1FAIpQLSdQOASsuKROvkQ1NSae5FvLi5rA2Mtn5zKqWIOKPadVbZkejg/viewform?embedded=true" width="640" height="833" frameborder="0">Loading…</iframe>
-                </div>
+                <div className="section-title">Contact Us</div>
+                <ul>
+                    <li className="contact-page_section">
+                        <div className="contact-section-title">Sign Up For Our Mailing List</div>
+                        <form name="subscribe-form" action="https://formspree.io/f/mpzgezzr" method="post">
+                            <label htmlFor="full-name">Full Name</label>
+                            <input type="text" name="name" id="full-name" required="" />
+                            <label htmlFor="email-address">Email Address</label>
+                            <input type="email" name="_replyto" id="email-address" required="" />
+                            <div className="contact-title">Please select the mailing lists you're interested in joining:</div>
+                            <div className="checkbox"> 
+                                <input type="checkbox" id="auditions" name="auditions" value="Auditions" />
+                                <label htmlFor="auditions"> Auditions</label>
+                            </div>
+                            <div className="checkbox">
+                                <input type="checkbox" id="performances" name="performances" value="Performances" />
+                                <label htmlFor="performances"> Upcoming Events / Performances</label>
+                            </div>
+                            <input type="submit" value="Submit" />
+                        </form>
+                    </li>
+                    <li className="contact-page_section">
+                        <div className="contact-section-title">Send Us A Message</div>
+                        <form name="contact-form" action="https://formspree.io/f/mgejbeek" method="post">
+                                <label htmlFor="full-name">Full Name</label>
+                                <input type="text" name="name" id="full-name" required="" />
+                                <label htmlFor="email-address">Email Address</label>
+                                <input type="email" name="_replyto" id="email-address" required="" />
+                                <label htmlFor="message">Message</label>
+                                <textarea rows="5" name="message" id="message" placeholder="" required=""></textarea>
+                                <input type="submit" value="Submit" />
+                        </form>
+                    </li>
+                </ul>
+                
             </main>
         </Layout>
     )
