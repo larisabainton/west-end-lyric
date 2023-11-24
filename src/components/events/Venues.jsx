9b71029244b61de92490ab1e2cb57a727bccabac
Larisa Bainton <larisabainton@gmail.com>
2023-11-24 08:39:49 -0500

    change venue title if only one venue

--- src/components/events/Venues.jsx
+++ src/components/events/Venues.jsx
@@ -2,9 +2,11 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import React from "react";
 
 const Venues = ({ events }) => {
+    const oneVenue = events.every(({ venue }) => venue.name === events[0].venue.name)
+
     return (
         <div className="production_venues" id="production_venues">
-            <div className="production_venues_title section-title">Venues</div>
+            <div className="production_venues_title section-title">{ oneVenue ? "Venue" : "Venues" }</div>
             <ul className="production_venues_list">
                 {events.sort((event1,event2) => new Date(event1.eventDate) - new Date(event2.eventDate))
                         .map(({ venue, eventDate }, i) => {
